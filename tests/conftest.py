8f67663d38a58f5b229a5af27097498c480ee0f8
Viktoria <48157182+violin88@users.noreply.github.com>
2024-01-13 15:12:20 +0400

    added utils

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from selenium import webdriver
 def browser_cfg():
     browser.config.base_url = 'https://demoqa.com'
     driver_options = webdriver.ChromeOptions()
-   # driver_options.add_argument('--headless')
+    driver_options.add_argument('--headless')
     browser.config.driver_options = driver_options
     browser.config.timeout = 5
     browser.config.window_height = 1920
@@ -16,3 +16,6 @@ def browser_cfg():
     yield
 
     browser.quit()
+
+
+
--- tests/test_form_demoqa.py
+++ tests/test_form_demoqa.py
@@ -1,44 +1,99 @@
+import os.path
+from datetime import datetime
+from selene import browser, have, be
 
-from selene import browser, have, be, by
-import os
 def test_form():
 
     browser.open('/automation-practice-form')
 
-    browser.element('#firstName').type('Vika')
-    browser.element('#lastName').type('Islentyeva')
-    browser.element('#userEmail').type('testik@mail.ru')
-    browser.element('#gender-radio-1').double_click()
-    browser.element('#userNumber').type('89376541238')
-    browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__month-select').click().element(
-        by.text('June')).click()
-    browser.element('.react-datepicker__day--025').click()
-    browser.element('#subjectsInput').should(be.blank).type('English').press_enter()
-    browser.element('[for="hobbies-checkbox-2"]').click()
-    browser.element('#uploadPicture').send_keys(os.path.abspath(
-        'resources/images.jpeg'))
+    browser.element('#userName-label').should(have.exact_text('Name'))
+    browser.element('#firstName').should(be.blank)
+    browser.element('#firstName').should(have.attribute('placeholder').value('First Name'))
+    browser.element('#firstName').type('Vika').press_enter()
 
-    browser.element('#currentAddress').should(be.blank).type('Moscow, Lenin street 42')
-    browser.element('#react-select-3-input').type('Uttar Pradesh').press_enter()
-    browser.element('#react-select-4-input').type('Agra').press_enter()
+
+
+
+    browser.element('#lastName').should(be.blank)
+    browser.element('#lastName').should(have.attribute('placeholder').value('Last Name'))
+    browser.element('#lastName').type('Islentyeva').press_enter()
+
+    browser.element('#userEmail-label').should(have.exact_text('Email'))
+    browser.element('#userEmail').should(be.blank)
+    browser.element('#userEmail').should(have.attribute('placeholder').value('name@example.com'))
+    browser.element('#userEmail').type('testik@mail.ru').press_enter()
+
+
+
+    browser.element('#genterWrapper > div.col-md-3.col-sm-12').should(have.exact_text('Gender'))
+    browser.all('[for*=gender-radio]').element_by(have.text('Female')).click()
+
+
+    browser.element('#userNumber-label').should(have.exact_text('Mobile(10 Digits)'))
+    browser.element('#userNumber-label > small').should(have.exact_text('(10 Digits)'))
+    browser.element('#userNumber').should(be.blank)
+    browser.element('#userNumber').should(have.attribute('placeholder').value('Mobile Number'))
+    browser.element('#userNumber').type('89376541238')
+
+
+    browser.element('#dateOfBirth-label').should(have.exact_text('Date of Birth'))
+    browser.element('#dateOfBirthInput').should(have.value(datetime.now().strftime("%d %b %Y")))
+    browser.element('#dateOfBirthInput').click()
+
+    browser.all('.react-datepicker__month-select>option').element_by(have.exact_text('June')).click()
+    browser.all('.react-datepicker__year-select>option').element_by(have.exact_text('1989')).click()
+    browser.all('*.react-datepicker__day').element_by(have.text('15')).click()
+    browser.element('#dateOfBirthInput').should(have.value('15 Jun 1989'))
+
+
+    browser.element('#subjects-label').should(have.exact_text('Subjects'))
+    browser.element('#subjectsInput').should(be.blank)
+    browser.element('#subjectsInput').type('Computer Science').press_enter()
+
+    browser.element('[for="hobbies-checkbox-1"]').should(have.exact_text('Sports'))
+    browser.element('[for="hobbies-checkbox-2"]').should(have.exact_text('Reading'))
+    browser.element('[for="hobbies-checkbox-3"]').should(have.exact_text('Music'))
+    browser.element('[for="hobbies-checkbox-1"]').click()
+    browser.element('[for="hobbies-checkbox-2"]').click()
+
+    browser.element('[for="uploadPicture"]').should(have.exact_text('Select picture'))
+    browser.element('#uploadPicture').send_keys(os.path.abspath('./resources/images.jpeg'))
+
+    # browser.element('[for="hobbies-checkbox-2"]').click()
+    # browser.element('#uploadPicture').send_keys(os.path.abspath(
+    #     'resources/images.jpeg'))
+
+    browser.element('#currentAddress-label').should(have.exact_text('Current Address'))
+    browser.element('#currentAddress').should(be.blank)
+    browser.element('#currentAddress').should(have.attribute('placeholder').value('Current Address'))
+    browser.element('#currentAddress').type('Moscow')
+    browser.element('#currentAddress').should(have.value('Moscow'))
+
+    browser.element('#stateCity-label').should(have.exact_text('State and City'))
+    browser.element('#state > div > div.css-1hwfws3 > div.css-1wa3eu0-placeholder').should(
+        have.exact_text('Select State'))
+    browser.element('#state > div > div.css-1wy0on6').click()
+    browser.element('#react-select-3-input').type('NCR').press_enter()
+    browser.element('#city > div > div.css-1hwfws3 > div.css-1wa3eu0-placeholder').should(
+        have.exact_text('Select City'))
+    browser.element('#react-select-4-input').type('Noida').press_enter()
+
     browser.element('#submit').press_enter()
 
-    browser.element('.modal-header').should(have.text('Thanks for submitting the form'))
 
-    browser.all('[class=table-responsive]').should(have.texts(
+    # browser.element('.table-bordered').all('td').even.should(have.exact_texts(
+    #     'Vika Islentyeva',
+    #     'testik@mail.ru',
+    #     'Female',
+    #     '89376541238',
+    #     '25 June,2023',
+    #     'English',
+    #     'Reading',
+    #     'images.jpg',
+    #     'Moscow',
+    #     'Noida'
+    #     ))
 
 
-        'Vika Islentyeva' and
-        'testik@mail.ru' and
-        'Female' and
-        '89376541238' and
-        '11 June,1996' and
-        'English' and
-        'Reading' and
-        'images.jpeg' and
-        'Moscow, Lenin street 42' and
-        'Uttar Pradesh Agra'
-    ))
 
-    #browser.element('#closeLargeModal').press_enter()
+
